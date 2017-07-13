8593de173c6467b42b249aa038ad1e3b9a4e88de
ASUS <ASUS@10.8.106.57>
2017-07-13 15:59:31 +0700

    koneksi

--- Randi.javacoder/src/Connection/Main.java
+++ Randi.javacoder/src/Connection/Main.java
@@ -1,14 +1,19 @@
 package Connection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
 	public static void main(String[] args) throws SQLException{
-		Connection koneksi = Koneksi.getKoneksi(); 
+		Connection koneksi = Koneksi.getKoneksi(); /*
 		Statement st = koneksi.createStatement();
-		st.executeUpdate("INSERT INTO xsis_employee VALUES (3, 'Asziz', 'Sragen', 3000)");
+		st.executeUpdate("UPDATE xsis_employee set nama = 'Randi' where id = 1");*/
+		PreparedStatement smst = koneksi.prepareStatement("UPDATE xsis_employee set nama = ? where id = ?");
+		smst.setString(1, "Asziz Jum'at");
+		smst.setInt(2, 2);
+		smst.executeUpdate();
 		System.out.println("row instead");
 	}
 }
